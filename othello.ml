363c7ae70bd6f3a78eab056e5e7fcb8d55cb0b10
YellowSquido <jujumasiee@gmail.com>
2025-12-12 16:05:29 +0100

    e

--- othello.ml
+++ othello.ml
@@ -3,9 +3,19 @@ type othello int array array * int
 
 let init_plat = 
   let tab = Array.make 8*8 0 in
-  tab.(3).(3) <-
+  tab.(3).(3) <- 2 ; tab.(3).(4)<- 1 ; tab.(4).(3)<-1 ; tab.(4).(4)<-2
   (tab,1)
 
 let jouer (etat:othello) i j = 
   let p,index = othello in 
-  assert 
+  assert( i >= 0 && i <= 7 && j >=0 && j<=7);
+  let 
+
+
+
+
+
+
+
+let ensemble_coups_possibles othello =
+  ()
